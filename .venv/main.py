0b3dc1013e5889fe63416583a80aab224a4e82d2
EvilCrake <48925014+EvilCrake@users.noreply.github.com>
2024-05-07 12:10:25 +0300

    homework

---
+++ .venv/main.py
@@ -0,0 +1,38 @@
+
+# x = input('Enter a number: ')
+# print('Здраствуйте')
+# if x < 0:
+#     print('Меньше нуля')
+# print('Досвидания')
+
+# a, b = 10, 5
+#
+# if a > b:
+#     print('a>b')
+#
+# if a < b and a > 0:
+#     print('успех')
+#
+# if (a > b) and (a < b or b < 1000):
+#     print('успех')
+#
+# if 5 < b and b < 10:
+#     print('успех')
+
+# if '34' > '123':
+#     print('успех')
+#
+# if '123' > '12':
+#     print('успех')
+#
+# if [1, 2] > [1, 1]:
+#     print('успех')
+
+if '6' > 5:
+    print('успех')
+
+if [5, 6] > 5:
+    print('успех')
+
+if '6' != 5:
+    print('успех')
